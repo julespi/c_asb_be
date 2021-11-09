61d413086157741805670d6625fe6636947e8005
Julian Spinelli <julespi@gmail.com>
2021-11-09 16:54:51 -0300

    clase 36

---
+++ src/main/java/com/julespi/springbootapirest/models/entity/Client.java
@@ -0,0 +1,39 @@
+package com.julespi.springbootapirest.models.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="clients")
+public class Client implements Serializable {
+
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Getter
+    @Setter
+    private String email;
+
+    @Getter
+    @Setter
+    @Temporal(TemporalType.DATE)
+    private Date created;
+
+
+}
