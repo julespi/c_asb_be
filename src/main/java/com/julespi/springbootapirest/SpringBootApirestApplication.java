bfa2b4d45471b82877a39ae497e3ba855418ce79
Julian Spinelli <julespi@gmail.com>
2021-11-09 16:23:11 -0300

    repo init

---
+++ src/main/java/com/julespi/springbootapirest/SpringBootApirestApplication.java
@@ -0,0 +1,13 @@
+package com.julespi.springbootapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApirestApplication.class, args);
+	}
+
+}
