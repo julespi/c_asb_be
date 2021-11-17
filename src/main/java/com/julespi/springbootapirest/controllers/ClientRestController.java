6d7ffa98cc7a54b890a29e5b49f55a3802c2542f
Julian Spinelli <julespi@gmail.com>
2021-11-17 16:42:36 -0300

    clase 60

--- src/main/java/com/julespi/springbootapirest/controllers/ClientRestController.java
+++ src/main/java/com/julespi/springbootapirest/controllers/ClientRestController.java
@@ -3,10 +3,14 @@ package com.julespi.springbootapirest.controllers;
 import com.julespi.springbootapirest.models.entity.Client;
 import com.julespi.springbootapirest.models.services.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -17,24 +21,37 @@ public class ClientRestController {
     private IClientService clientService;
 
     @GetMapping("/clients")
-    public List<Client> index(){
+    public List<Client> index() {
         return clientService.findAll();
     }
 
     @GetMapping("/clients/{id}")
-    public Client show(@PathVariable Long id){
-        return clientService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Client client = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            client = clientService.findById(id);
+        }catch (DataAccessException e){
+            response.put("message", "Error while querying database.");
+            response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (client == null) {
+            response.put("message", "Client with id: ".concat(id.toString()).concat(" does not exist."));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Client>(client, HttpStatus.OK);
     }
 
     @PostMapping("/clients")
     @ResponseStatus(HttpStatus.CREATED)
-    public Client create(@RequestBody Client client){
+    public Client create(@RequestBody Client client) {
         return clientService.save(client);
     }
 
     @PutMapping("/clients/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Client update(@RequestBody Client client, @PathVariable Long id){
+    public Client update(@RequestBody Client client, @PathVariable Long id) {
         Client actualClient = clientService.findById(id);
         actualClient.setName(client.getName());
         actualClient.setLast_name(client.getLast_name());
@@ -44,7 +61,7 @@ public class ClientRestController {
 
     @DeleteMapping("/clients/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         clientService.delete(id);
     }
 }
