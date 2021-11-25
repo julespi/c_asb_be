319bb96a3a9b48389fac40daef5dfc592178eaed
Julian Spinelli <julespi@gmail.com>
2021-11-25 16:37:10 -0300

    agregando el locale para los mensajes de validacion

--- src/main/java/com/julespi/springbootapirest/SpringBootApirestApplication.java
+++ src/main/java/com/julespi/springbootapirest/SpringBootApirestApplication.java
@@ -3,11 +3,14 @@ package com.julespi.springbootapirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class SpringBootApirestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApirestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);    //changes the default language
+        SpringApplication.run(SpringBootApirestApplication.class, args);
+    }
 
 }
